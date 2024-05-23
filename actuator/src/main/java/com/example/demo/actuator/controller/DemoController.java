0c094a5887a3d63651eae0debedfc1d506e1d6ed
zt <zhtao01@163.com>
2024-05-23 16:46:20 +0800

    init

---
+++ actuator/src/main/java/com/example/demo/actuator/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.example.demo.actuator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author zhangtao
+ * @since 2024/5/7 19:17
+ */
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+    @RequestMapping("/echo")
+    @ResponseBody
+    public String echo() {
+        return "hello";
+    }
+
+    @RequestMapping("/error")
+    @ResponseBody
+    public String error() {
+        throw new RuntimeException();
+    }
+}
---
+++ actuator/src/main/java/com/example/demo/actuator/service/DemoService.java
@@ -0,0 +1,14 @@
+package com.example.demo.actuator.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhangtao
+ * @since 2024/5/7 22:50
+ */
+@Service
+public class DemoService {
+    public void say(){
+        System.out.println("hello");
+    }
+}
